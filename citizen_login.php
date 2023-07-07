db6e29f53fe17d769ef540617840c12f69ed7052
jamesrussellsaro <78161240+jamesrussellsaro@users.noreply.github.com>
2023-07-07 16:31:42 +0800

    Add files via upload
    working archive button for pending and complete complaints (debug for processing)

--- citizen_login.php
+++ citizen_login.php
@@ -9,17 +9,17 @@ if ($conn->connect_error) {
 $cellphoneNumber = $_POST['cellphoneNumber'];
 $password = $_POST['password'];
 
-$sql = "SELECT * FROM user WHERE cellphoneNumber = '$cellphoneNumber' AND password = '$password' AND accountType = 'Barangay Operator'";
+$sql = "SELECT * FROM user WHERE cellphoneNumber = '$cellphoneNumber' AND password = '$password' AND accountType = 'Citizen'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
 
-    //START SESSION HERE 
-    $_SESSION['sessionBrgyOperatorID'] = $row['userID'];
-    header("Location: admin.php");
+    $_SESSION['citizenID'] = $row['userID'];
+    header("Location: citizen_home.php");
 } else {
     echo "Invalid cellphone number or password.";
+    echo "<a href='citizen_login_page.html'> Back to Login</a>";
 }
 
 $conn->close();
---
+++ session_error_page_LGU.php
@@ -0,0 +1,6 @@
+<?php 
+
+echo "You are not logged in! <br/>";
+echo "<a href='lgu_operator_login_page.php'>Back to login</a>";
+
+?>
